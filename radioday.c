7475f5a43b6b43cde8c03d8852d62dfd4387a6a5
Bjorn Ekelund <bjorn@ekelund.nu>
2019-03-07 18:18:44 +0100

    App now recognize current time

--- radioday.c
+++ radioday.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "sunriset.h"
 
 int main(int argc, char *argv[])
@@ -22,8 +23,10 @@ int main(int argc, char *argv[])
 	int normS, normT, UTs, UTr; // In minutes
 	int grayD = 180; // In minutes
 	int dayLen, riseDist, setDist; // In minutes
-	int rs;
+	int rs; // Status from sunset/sunrise calculation
 	int verbose = 0;
+	time_t rawtime;
+	struct tm *info;
 
 	if (argc < 2 ||
 		strlen(argv[1]) != 6 ||
@@ -50,6 +53,12 @@ int main(int argc, char *argv[])
 		grayD = abs(grayD);
 	}
 
+	time(&rawtime);
+	info = gmtime(&rawtime);
+	year = info->tm_year + 1900;
+	month = info->tm_mon + 1;
+	day = info->tm_mday;
+	curT = info->tm_hour * 60 + info->tm_min;
 
 //	Take time from command line if it's there
 	if (argc > 3) {
@@ -66,23 +75,22 @@ int main(int argc, char *argv[])
 		month = atoi(sMonth);
 		strncpy(sDay, argv[4] + 6, 2);
 		day = atoi(sDay);
-		if (verbose)
-			printf("sYear=\"%s\", sMonth=\"%s\", sDay=\"%s\"\n", sYear, sMonth, sDay);
 	}
 
-	rs = sun_rise_set(year, month, day, longitude, latitude, &UTrH, &UTsH);
 	latitude = (grid[1] - 65) * 10 + (grid[3] - 48) + ((double)grid[5] - 65.) / 24 + 1. / 48 - 90;
 	longitude = (grid[0] - 65) * 20 + (grid[2] - 48) * 2 + ((double)grid[4] - 65.) / 12 + 1. / 24 - 180;
+
+	rs = sun_rise_set(year, month, day, longitude, latitude, &UTrH, &UTsH);
+
 	UTs = round(60.0 * UTsH);
 	UTr = round(60.0 * UTrH);
 
 	if (verbose) {
-		printf("Current time in decimal: %f\n", curT / 60.0);
-		printf("Grayline duration in decimal: %.1fh\n", grayD / 60.0);
-		printf("Grid locator: %s\n", grid);
-		printf("Latitude = %f deg. Longitude = %f deg. Date: %04d-%02d-%02d\n",
-			latitude, longitude, year, month, day);
-		printf("rs = %d\n", rs);
+		printf("Date: %4d-%02d-%02d Time: %02d:%02d ", year, month, day,
+			(int)(curT / 60.0), (int)round(60 * (curT / 60.0 - floor(curT / 60.0))));
+		printf("Twilight duration: %.2fh\n", grayD / 60.0);
+		printf("Grid locator: %s ==> ", grid);
+		printf("Latitude = %.4f deg. Longitude = %.4f deg.\n", latitude, longitude);
 	}
 
 
@@ -97,30 +105,42 @@ int main(int argc, char *argv[])
 		else
 			printf("night\n");
 	else {
+		if (verbose)
+			printf("Normal day/night cycle.\n");
+
 		dayLen = (UTs - UTr + 1440) % 1440;
 
+		// Calculate distance to sunrise
 		riseDist = (int)(curT - UTr + 2880) % 1440;
 		riseDist = (riseDist > 720) ? 1440 - riseDist : riseDist;
 
+		// Calculate distance to sunset
 		setDist =  (int)(curT - UTs + 2880) % 1440;
 		setDist = (setDist > 720) ? 1440 - setDist : setDist;
 
-		normT = (curT - UTr + 1440) % 1440;
+		// Calculate current time relative to sunrise
+		normT = (curT - UTr + 1440) % 1440; 
 
 		if (setDist < grayD / 2.0 || riseDist < grayD / 2.0)
+			// We are within grayD/2 of sunset or sunrise
 			state = "gray";
 		else if (normT > 0 && normT < dayLen)
+			// We are after sunrise and before sunset
 			state = "day";
 		else
+			// Else its night
 			state = "night";
 
 		if (!verbose)
 			printf("%s\n", state);
 		else {
-			printf("CurT: %05.2f normT: %5.2f rDist: %05.2f sDist: %05.2f State: %s\n",
-				curT / 60.0, normT / 60.0, riseDist / 60.0, setDist / 60.0, state);
-			printf("Distance to sunrise: %.2f to sunset: %.2f\n", riseDist / 60.0, setDist / 60.0);
-			printf("Sunrise: %5.2f Sunset: %5.2f\n", UTrH, UTsH);
+//			printf("CurT: %05.2f normT: %5.2f rDist: %05.2f sDist: %05.2f State: %s\n",
+//				curT / 60.0, normT / 60.0, riseDist / 60.0, setDist / 60.0, state);
+			printf("Sunrise: %02d:%02d Sunset: %02d:%02d\n", 
+				(int)(UTrH), (int)round(60 * (UTrH - floor(UTrH))),
+				(int)(UTsH), (int)round(60 * (UTsH - floor(UTsH))));
+			printf("Time from sunrise: %.2fh from sunset: %.2fh\nSolar state: %s\n", 
+				riseDist / 60.0, setDist / 60.0, state);
 		}
 	}
 
